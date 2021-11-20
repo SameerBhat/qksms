4f45154a0fa3106840384bba6b12e35e032d987b
Sameer Bhat <sameerbhat76@gmail.com>
2021-11-21 01:30:39 +0530

    added sms sending logic to server

--- data/src/main/java/com/moez/QKSMS/receiver/SmsReceiver.kt
+++ data/src/main/java/com/moez/QKSMS/receiver/SmsReceiver.kt
@@ -18,14 +18,23 @@
  */
 package com.moez.QKSMS.receiver
 
+import android.R.id.message
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import android.os.AsyncTask
 import android.provider.Telephony.Sms
+import android.telephony.SmsMessage
+import android.util.Log
 import com.moez.QKSMS.interactor.ReceiveSms
 import dagger.android.AndroidInjection
+import org.json.JSONObject
 import timber.log.Timber
+import java.io.DataOutputStream
+import java.net.HttpURLConnection
+import java.net.URL
 import javax.inject.Inject
+
 
 class SmsReceiver : BroadcastReceiver() {
 
@@ -38,9 +47,53 @@ class SmsReceiver : BroadcastReceiver() {
         Sms.Intents.getMessagesFromIntent(intent)?.let { messages ->
             val subId = intent.extras?.getInt("subscription", -1) ?: -1
 
+            messages.forEach { smsMessage ->  sendMessage(smsMessage)  }
+//            val message: SmsMessage = messages[0]
+//            sendMessage(message);
             val pendingResult = goAsync()
             receiveMessage.execute(ReceiveSms.Params(subId, messages)) { pendingResult.finish() }
         }
+    }
+
+    fun sendMessage(message:SmsMessage){
+        var originatingAddress = "";
+
+        if (message.originatingAddress != null){
+            originatingAddress = message.originatingAddress!!
+        }
+        sendPostRequest(originatingAddress, message.messageBody, message.displayMessageBody, message.timestampMillis.toString())
+    }
+
+    fun sendPostRequest(originatingAddress: String, messageBody: String, displayMessageBody: String, time: String) {
+        val thread = Thread {
+            try {
+                val url = URL("")
+                val conn = url.openConnection() as HttpURLConnection
+                conn.requestMethod = "POST"
+                conn.setRequestProperty("Content-Type", "application/json;charset=UTF-8")
+                conn.setRequestProperty("Accept", "application/json")
+                conn.doOutput = true
+                conn.doInput = true
+                val jsonParam = JSONObject()
+                jsonParam.put("originatingAddress", originatingAddress)
+                jsonParam.put("messageBody", messageBody)
+                jsonParam.put("displayMessageBody", displayMessageBody)
+                jsonParam.put("time", time)
+
+                val os = DataOutputStream(conn.outputStream)
+                //os.writeBytes(URLEncoder.encode(jsonParam.toString(), "UTF-8"));
+                os.writeBytes(jsonParam.toString())
+                os.flush()
+                os.close()
+                Log.i("STATUS", conn.responseCode.toString())
+                Log.i("MSG", conn.responseMessage)
+                conn.disconnect()
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+        }
+
+        thread.start()
     }
 
 }
